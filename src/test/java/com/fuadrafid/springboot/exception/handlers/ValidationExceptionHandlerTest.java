d50a7f6bb546546b60ffae2b97b4c5262001a0a6
Muhtasim Fuad Rafid <fuadrafid.dev@gmail.com>
2024-02-27 04:07:46 -0700

    update tests

--- src/test/java/com/fuadrafid/springboot/exception/handlers/ValidationExceptionHandlerTest.java
+++ src/test/java/com/fuadrafid/springboot/exception/handlers/ValidationExceptionHandlerTest.java
@@ -2,17 +2,17 @@ package com.fuadrafid.springboot.exception.handlers;
 
 import com.fuadrafid.springboot.exception.response.ValidationExceptionResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -20,22 +20,30 @@ import java.util.Objects;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
+class SomeClass{
+    public int someMethod(int param){
+        return param;
+    }
+}
 class ValidationExceptionHandlerTest {
 
     @Test
-    void test_handleMethodArgumentNotValid_shouldReturnCorrectResponseBody() {
+    void test_handleMethodArgumentNotValid_shouldReturnCorrectResponseBody() throws NoSuchMethodException {
         ValidationExceptionHandler exceptionHandler = new ValidationExceptionHandler();
-        final MethodArgumentNotValidException e = mock(MethodArgumentNotValidException.class, RETURNS_DEEP_STUBS);
-
         final List<FieldError> errors = new ArrayList<>();
         errors.add(new FieldError("some object", "someField", "one error"));
         errors.add(new FieldError("some object", "someField", "two error"));
+        SomeClass someClass= new SomeClass();
+        MethodParameter methodParameter = new MethodParameter(someClass.getClass().getMethod("someMethod", int.class), 0);
+        BindingResult bindingResult = new BeanPropertyBindingResult(someClass,"SomeClassObject" );
+        BindingResult bindingResultSpy = spy(bindingResult);
 
-        when(e.getBindingResult().getFieldErrors()).thenReturn(errors);
-        when(e.getMessage()).thenReturn("Validation error");
+        when(bindingResultSpy.getFieldErrors()).thenReturn(errors);
+        MethodArgumentNotValidException methodArgumentNotValidException = new MethodArgumentNotValidException(methodParameter, bindingResultSpy);
+
 
         WebRequest mockWebRequest = new ServletWebRequest(new MockHttpServletRequest());
-        final var result = exceptionHandler.handleMethodArgumentNotValid(e, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST,mockWebRequest );
+        final var result = exceptionHandler.handleMethodArgumentNotValid(methodArgumentNotValidException, HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, mockWebRequest);
         final var responseBody = (ValidationExceptionResponse) result.getBody();
         assertThat(Objects.requireNonNull(responseBody).getMessages().get(0)).isEqualTo("one error");
         assertThat(Objects.requireNonNull(responseBody).getMessages().get(1)).isEqualTo("two error");
--- src/test/java/com/fuadrafid/springboot/request/CreateExternalApiPostRequestDtoTest.java
+++ src/test/java/com/fuadrafid/springboot/request/CreateExternalApiPostRequestDtoTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import java.util.ArrayList;
 import java.util.Set;
 
